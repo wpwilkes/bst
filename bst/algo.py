2ea619cb1d6a59f32e9033cde2db1f19cd81299f
Wesley Wilkes <wwilkes@rpagency.com>
2022-04-30 10:58:39 -0500

    added documentation

--- bst/algo.py
+++ bst/algo.py
@@ -8,8 +8,39 @@ import builtins
 from bst.node import Node
 
 
+def depth(node: Optional[Node]) -> int:
+    """
+    Compute the depth of a node.
+
+    Parameters
+    ----------
+    node : node, optional
+        The node whose depth will be computed.
+
+    Returns
+    -------
+    int :
+        The node's depth.
+    """
+    if node is None:
+        return 0
+    else:
+        return 1 + depth(node.parent)
+
+
 def height(node: Optional[Node]) -> int:
     """
+    Compute the height of a node.
+
+    Parameters
+    ----------
+    node : node, optional
+        The node whose height will be computed.
+
+    Returns
+    -------
+    int :
+        The node's height.
     """
     if node is None:
         return 0
@@ -22,6 +53,14 @@ def height(node: Optional[Node]) -> int:
 def inorder_traverse(node: Optional[Node],
                      visit: Callable[[Node], Any]) -> None:
     """
+    Traverse any tree rooted at the given node in an inorder manner.
+
+    Parameters
+    ----------
+    node : optional, node
+        The node to begin the traversal at.
+    visit : callable
+        The visit action to perform at the nodes.
     """
     if node is None:
         return None
@@ -30,40 +69,55 @@ def inorder_traverse(node: Optional[Node],
     inorder_traverse(node.right, visit)
 
 
-def max(node: Optional[Node]) -> Optional[Node]:
+def max(node: Node) -> Node:
     """
+    Find the node with max key for the tree rooted at the given node.
+
+    Parameters
+    ----------
+    node : Node
+        The root of a tree.
+
+    Returns
+    -------
+    node : Node
+        The node with max key.
     """
-    if node is None:
-        return None
     while node.right is not None:
         node = node.right
     return node
 
 
-def min(node: Optional[Node]) -> Optional[Node]:
+def min(node: Node) -> Node:
     """
+    Find the node with min key for the tree rooted at the given node.
+
+    Parameters
+    ----------
+    node : Node
+        The root of a tree.
+
+    Returns
+    -------
+    node : Node
+        The node with min key.
     """
-    if node is None:
-        return None
     while node.left is not None:
         node = node.left
     return node
 
 
-def preorder_traverse(node: Optional[Node],
-                      visit: Callable[[Node], Any]) -> None:
-    """
-    """
-    if node is None:
-        return None
-    visit(node)
-    preorder_traverse(node.left, visit)
-    preorder_traverse(node.right, visit)
-
-
 def postorder_traverse(node: Optional[Node],
                        visit: Callable[[Node], Any]) -> None:
     """
+    Traverse any tree rooted at the given node in a postorder manner.
+
+    Parameters
+    ----------
+    node : optional, node
+        The node to begin the traversal at.
+    visit : callable
+        The visit action to perform at the nodes.
     """
     if node is None:
         return None
@@ -72,8 +126,40 @@ def postorder_traverse(node: Optional[Node],
     visit(node)
 
 
+def preorder_traverse(node: Optional[Node],
+                      visit: Callable[[Node], Any]) -> None:
+    """
+    Traverse any tree rooted at the given node in a preorder manner.
+
+    Parameters
+    ----------
+    node : optional, node
+        The node to begin the traversal at.
+    visit : callable
+        The visit action to perform at the nodes.
+    """
+    if node is None:
+        return None
+    visit(node)
+    preorder_traverse(node.left, visit)
+    preorder_traverse(node.right, visit)
+
+
 def search(node: Optional[Node], key: int) -> Optional[Node]:
     """
+    Perform binary search starting at the tree rooted at given node.
+
+    Parameters
+    ----------
+    node : Node, optional
+        The node to begin searching at.
+    key : int
+        The key to search for.
+
+    Returns
+    -------
+    Node, optional :
+        The node if it is found; otherwise, None.
     """
     if node is None:
         return None
@@ -86,6 +172,24 @@ def search(node: Optional[Node], key: int) -> Optional[Node]:
 
 
 def search_range(node: Optional[Node], min_key: int, max_key: int):
+    """
+    Perform a range query at the tree rooted at the given node.
+
+    Parameters
+    ----------
+    node : Node, optional
+        The node to begin searching at.
+    min_key : int
+        The minimum key to include in the range.
+    max_key : int
+        The maximum key to include in the range.
+
+    Returns
+    -------
+    list :
+        A list of nodes with keys in the range of (min_key, max_key)
+        if any exist. Otherwise, an empty list is returned.
+    """
     if node is None:
         return []
     if (min_key <= node.key) and (node.key <= max_key):
